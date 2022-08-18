33cf3036e950a1997057c48131c3cc3b8358f1b5
Your Name <minoyuu1294@gmail.com>
2022-08-19 01:20:58 +0700

    Use the if-command to reject the case of b = 0

--- divide_two_number.py
+++ divide_two_number.py
@@ -1,5 +1,5 @@
 def divide(a,b):
     if b==0:
-        print("Zero Division Error!\nDivision cannot be done because b = 0.")
+        print("Division cannot be done because b = 0.")
     else:
         return a/b
